61962ee3e61ae43f60bd85a92e70c5af73a202e2
Arya Rakha <arrakhpra@gmail.com>
2025-07-01 21:29:20 +0200

    started card components

---
+++ Assets/Scripts/Prototype/CardComponents/Implementations/RangedComponent.cs
@@ -0,0 +1,19 @@
+﻿
+using Utilities;
+
+namespace Prototype.CardComponents.Implementations
+{
+    
+    public class RangedComponent : ICardComponent
+    {
+        private int durability = 1;
+        
+        public string DisplayName => $"Ranged {durability}";
+        public string[] Aliases => new[] {"ranged"};
+        
+        public void Initialize(string[] args)
+        {
+            durability = args.GetInt(0, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/StringUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Utilities
+{
+    public static class StringUtilities
+    {
+        public static int GetInt(this string[] args, int argsPos = 1, int fallbackValue = 0)
+        {
+            if (args.Length <= argsPos || !int.TryParse(args[argsPos], out int amount)) return fallbackValue;
+            return amount;
+        }
+
+        public static T GetEnum<T>(this string[] args, int argsPos = 1, T fallbackValue = default) where T : struct
+        {
+            if (args.Length <= argsPos || !Enum.TryParse<T>(args[argsPos], true, out var e)) return fallbackValue;
+            return e;
+        }
+    }
+}
