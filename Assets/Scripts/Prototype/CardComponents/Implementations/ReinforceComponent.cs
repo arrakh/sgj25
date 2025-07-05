1b4c0f6cb2c04b065fe32123f55b9ab4e34a826a
Raka Arya Pratama <raka.arya34@gmail.com>
2025-07-05 19:54:36 +0700

    localization cards components

--- Assets/Scripts/Prototype/CardComponents/Implementations/ReinforceComponent.cs
+++ Assets/Scripts/Prototype/CardComponents/Implementations/ReinforceComponent.cs
@@ -1,11 +1,17 @@
-﻿namespace Prototype.CardComponents.Implementations
+﻿/*
+ *   Copyright (c) 2025 
+ *   All rights reserved.
+ */
+ 
+ using Assets.SimpleLocalization.Scripts;
+namespace Prototype.CardComponents.Implementations
 {
     public class ReinforceComponent : CardComponent, IOnItemUse
     {
-        public override string DisplayName => "Reinforce";
+        public override string DisplayName => LocalizationManager.Localize("reinforce-title");
 
-        public override string Description => $"Adds {cardInstance.Data.value} to equipped weapon. Nothing happens if no weapon is equipped";
-        public override string[] Aliases => new []{"reinforce"};
+        public override string Description => LocalizationManager.Localize("reinforce-description",cardInstance.Data.value);
+        public override string[] Aliases => new[] { "reinforce" };
 
         public void OnItemUse(ArenaController arena)
         {
